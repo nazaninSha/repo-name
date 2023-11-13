628eca8560793e6f1fb9cd1b4919f247193c6df6
nazaninSha <nazanin.shafieikhatib@gmail.com>
2023-11-13 12:28:36 +1100

    eddited fils

--- Program.cs
+++ Program.cs
@@ -3,11 +3,32 @@ using repo_name.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 // After Add services to the container.
-builder.Services.AddDbContext<RoiDatabaseContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
+builder.Services.AddDbContext<RoiDatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Add after RoiDatabaseContext Service configuration
 
+// Define CORS policies to allow access to our API
+builder.Services.AddCors(options => {
+
+    // Build a default CORS policy
+    options.AddDefaultPolicy(policy => {
+
+        // Allow ANY origin to access our API
+        policy.AllowAnyOrigin();
+
+        // Allow SPECIFIC origins to access our API
+        //policy.WithOrigins("http://localhost:19006", "https://app.roi.com.au");
+
+        // Allow any HTTP header
+        policy.AllowAnyHeader();
+
+        // Allow any HTTP method
+        policy.AllowAnyMethod();
+
+    });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -24,7 +45,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+// Add between app.UseHttpsRedirection();  and app.UseAuthorization();
 
+// Enable CORS (default policy)
+app.UseCors();
 app.UseAuthorization();
 
 app.MapControllers();
